fd16a322a3ac322e854730b2bc465d54d23f143e
nemosminer <slbaker@hotmail.co.nz>
2018-11-28 14:11:59 +1300

    submit-stale shares

--- ccminer-config-win.h
+++ ccminer-config-win.h
@@ -156,7 +156,7 @@
 #define PACKAGE_NAME "ccminer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ccminer Yescrypt/R16/R32"
+#define PACKAGE_STRING "ccminer Yescrypt/R16/R32.V8"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ccminer"
@@ -165,7 +165,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "Yescrypt/R16/R32.V7"
+#define PACKAGE_VERSION "Yescrypt/R16/R32.V8"
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
